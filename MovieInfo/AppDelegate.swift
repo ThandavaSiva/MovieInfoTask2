cc5c36fa256b0d05fbf6ba163071d7a47465f258
AdminUser <adminuser@siva-tirukala-mac.local>
2018-09-27 12:43:52 +0530

    sample method

--- MovieInfo/AppDelegate.swift
+++ MovieInfo/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        sampleMetho()
         
+        //gfhhfghfghfg
         isServerRechable = false
         SSASwiftReachability.sharedManager?.startMonitoring()
         // MARK: Listen For Network Reachability Changes
@@ -67,6 +69,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+    func sampleMetho(){
+        
+        print("SampleMethod created")
+        
+    }
+    
+    
+    
+    
+    
 
 }
 
